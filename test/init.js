59739fd4599dd4f48400e730b8a5ab8cf91295e0
Juan Pablo Diaz-Vaz <jpdiazvaz@mcplusa.com>
2016-04-18 17:00:47 -0300

    LCGC-20 Set tests to run under a live environment

--- test/init.js
+++ test/init.js
@@ -2,13 +2,15 @@ module.exports = require('should');
 
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
+var nock = require('nock');
+nock.recorder.rec({dont_print:true,output_objects: false});
+
 var TEST_ENV = process.env.TEST_ENV || 'test';
 var config = require('rc')('loopback', {test: {gcloud: {}}})[TEST_ENV].gcloud;
 
 config = {
-  projectId: 'fakeProject',
-  keyFilename: '<keyfile>',
-  email: '<email>'
+  projectId: '<YOUR_PROJECT_ID>',
+  keyFilename: '<YOUR_JSON_KEY_PATH>'
 };
 
 global.config = config;
